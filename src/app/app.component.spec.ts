213a73749fd292719c0b54bc9217113360b6351e
claudinemahoro <claudinemah3@gmail.com>
2020-11-11 00:43:29 +0200

    import all modules to be used

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Github1'`, () => {
+  it(`should have as title 'githubSearch'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Github1');
+    expect(app.title).toEqual('githubSearch');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Github1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('githubSearch app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,71 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { NotfoundComponent } from './notfound/notfound.component';
+// import { SearchComponent } from './search/search.component';
+// import { StrikethroughDirective } from './strikethrough.directive';
+// import { DateCountPipe } from './date-count.pipe';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     NavbarComponent,
+//     ProfileComponent,
+//     NotfoundComponent,
+//     SearchComponent,
+//     StrikethroughDirective,
+//     DateCountPipe
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// import {Headers} from '@angular/common/http';
+import { ProfileService } from './profile.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
-import { NotfoundComponent } from './notfound/notfound.component';
 import { SearchComponent } from './search/search.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProfileComponent,
-    NotfoundComponent,
     SearchComponent,
+    NotfoundComponent,
     StrikethroughDirective,
     DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // Headers,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
