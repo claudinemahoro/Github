31ddad77359fdb8d2541234d39de7badb5fe37ab
claudinemahoro <claudinemah3@gmail.com>
2020-11-11 00:09:12 +0200

    create the strikethrough directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { SearchComponent } from './search/search.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SearchComponent } from './search/search.component';
     NavbarComponent,
     ProfileComponent,
     NotfoundComponent,
-    SearchComponent
+    SearchComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
